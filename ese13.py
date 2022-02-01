3896702bc4e6d5b8317db43aa3da487e2998a79f
AParente <parente.antonio@darzo.net>
2022-02-01 14:40:21 +0100

    commit

---
+++ ese13.py
@@ -0,0 +1,5 @@
+import math
+base = input('Inserire la base: ')
+espo = input('Inserire l\'esponente: ')
+ris = pow(int(base), int(espo))
+print('b^e: ' + str(ris))
---
+++ ese14.py
@@ -0,0 +1,4 @@
+import math
+e = input('Inserire un numero (> o): ')
+rad = math.sqrt(int(e))
+print('Radie quadrata: ' + str(rad))
